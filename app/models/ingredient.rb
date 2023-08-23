7809fa19d6d9b76d7486564177ff4e63e810ca33
Charles Z <charles.zhang@sentia.com.au>
2023-08-23 19:04:27 +1000

    Bugfix: extra characters on html partial removed

--- app/models/ingredient.rb
+++ app/models/ingredient.rb
@@ -1,5 +1,6 @@
 class Ingredient < ApplicationRecord
   before_validation :prevalidate
+  validates :name, presence: true, uniqueness: true
 
   has_many :combinations, dependent: :destroy, foreign_key: :first_ingredient_id
   has_many :mixes, through: :combinations, source: :second_ingredient
