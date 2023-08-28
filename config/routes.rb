34081df723ece1a0b14c19af7c6f5cdd28bd00bf
Charles Z <charles.zhang@sentia.com.au>
2023-08-28 22:20:19 +1000

    WIP; stimulus controller props

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,7 @@
 Rails.application.routes.draw do
   resources :posts, only: [ :index, :create ]
+  resources :synergies, only: [ :index ]
   root to: 'posts#index'
 
-  # get 'ingredients', to: 'ingredients#index'
-  # post 'ingredients', to: 'ingredients#create'
-  # delete 'ingredients', to: 'ingredients#delete'
   resources :ingredients, only: [ :index, :create, :update, :destroy ]
 end
-
-# resources :comments, only: [:create, :destroy] do
-#   member do
-#     patch "approve"
-#   end
-# end
